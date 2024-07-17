8de87ed37892a4e047240b39a12693f03d7d23ac
HCM00029\bp_NameDat <Dat.Tran@nttdata.com>
2024-07-17 17:59:31 +0700

    done

--- src/main/java/com/dat/miniATM/MiniAtmApplication.java
+++ src/main/java/com/dat/miniATM/MiniAtmApplication.java
@@ -1,14 +1,12 @@
 package com.dat.miniATM;
 
-import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
-import org.axonframework.springboot.autoconfig.JdbcAutoConfiguration;
-import org.axonframework.springboot.autoconfig.JpaAutoConfiguration;
-import org.axonframework.springboot.autoconfig.JpaEventStoreAutoConfiguration;
+import com.dat.miniATM.adapters.configuration.AxonMongoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = "org.axonframework.extensions.mongo.springboot.autoconfig",exclude = {AxonAutoConfiguration.class, JpaAutoConfiguration.class, JdbcAutoConfiguration.class, JpaEventStoreAutoConfiguration.class} )
+//@SpringBootApplication(scanBasePackages = "org.axonframework.extensions.mongo.springboot.autoconfig",exclude = {AxonAutoConfiguration.class, JpaAutoConfiguration.class, JdbcAutoConfiguration.class, JpaEventStoreAutoConfiguration.class} )
+@SpringBootApplication(scanBasePackageClasses = {AxonMongoConfig.class})
 public class MiniAtmApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dat/miniATM/adapters/configuration/AxonMongoConfig.java
+++ src/main/java/com/dat/miniATM/adapters/configuration/AxonMongoConfig.java
@@ -1,48 +1,50 @@
 package com.dat.miniATM.adapters.configuration;
 
-import com.mongodb.client.MongoClient;
+
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.Configurer;
-import org.axonframework.config.ConfigurerModule;
 import org.axonframework.config.DefaultConfigurer;
+
+import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
-import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
-import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
-import org.axonframework.eventsourcing.Snapshotter;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.MongoTemplate;
-import org.axonframework.extensions.mongo.eventhandling.deadletter.MongoSequencedDeadLetterQueue;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.documentperevent.DocumentPerEventStorageStrategy;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
-import org.axonframework.extensions.mongo.springboot.AxonMongoProperties;
-import org.axonframework.serialization.Serializer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.axonframework.mongo.DefaultMongoTemplate;
+import org.axonframework.mongo.MongoTemplate;
+import org.axonframework.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AxonMongoConfig {
 
-
     @Bean
+    public String hello() {
+        return "hello";
+    }
+
+
+
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://root:rootpassword@localhost:27017");
+        return MongoClient
+                .create("mongodb://root:rootpassword@localhost:27017");
     }
 
     @Bean
     public MongoTemplate axonMongoTemplate() {
+        new DefaultMongoTemplate(mongoClient());
         return DefaultMongoTemplate.builder()
                 .mongoDatabase(mongoClient(), "axon")
                 .build();
     }
 
-    @Autowired
-    public void configure(Configurer configurer, MongoClient client) {
-        configurer.configureEmbeddedEventStore(configuration -> storageEngine(client))
-                .eventProcessing(conf -> conf.registerTokenStore(config -> tokenStore(client, config.serializer())));
-    }
 
     @Bean
     public EventStorageEngine storageEngine(MongoClient client) {
@@ -51,23 +53,37 @@ public class AxonMongoConfig {
                 .build();
     }
 
+
     @Bean
-    public TokenStore tokenStore(MongoClient client, Serializer serializer) {
-        return MongoTokenStore.builder()
-                .mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build())
-                .serializer(serializer)
+    public MongoEventStorageEngine eventStorageEngine(MongoTemplate axonMongoTemplate) {
+        return MongoEventStorageEngine.builder()
+                .mongoTemplate(axonMongoTemplate)
+                .storageStrategy(new DocumentPerEventStorageStrategy())
                 .build();
     }
 
     @Bean
-    public SnapshotTriggerDefinition mySnapshotTriggerDefinition(org.axonframework.config.Configuration configuration) {
-        return new EventCountSnapshotTriggerDefinition(configuration.snapshotter(), 5);
+    public TokenStore tokenStore(MongoTemplate axonMongoTemplate) {
+        return MongoTokenStore.builder()
+                .mongoTemplate(axonMongoTemplate).build();
     }
 
     @Bean
-    public DefaultConfigurer configurer() {
-        return (DefaultConfigurer) DefaultConfigurer.defaultConfiguration();
+    public EmbeddedEventStore embeddedEventStore(MongoEventStorageEngine mongoEventStorageEngine) {
+        return new EmbeddedEventStore(mongoEventStorageEngine);
     }
 
+    @Bean
+    public Configurer configurer(EmbeddedEventStore embeddedEventStore, TokenStore tokenStore) {
+        Configurer configurer = DefaultConfigurer.defaultConfiguration();
+
+        configurer.configureEventStore(c -> embeddedEventStore);
+        configurer.registerComponent(TokenStore.class, c -> tokenStore);
+
+        configurer.buildConfiguration();
+        configurer.start();
+        return configurer;
+
+    }
 
 }
